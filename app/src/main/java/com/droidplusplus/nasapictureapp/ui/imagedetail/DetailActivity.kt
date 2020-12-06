d34b3f3a009a2bbd8efb14b217230232e868b537
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-07 00:45:19 +0530

    1. Image detail screen DetailActivity created. 2. Apply base abstraction on DetailActivity by extending BaseActivity.

---
+++ app/src/main/java/com/droidplusplus/nasapictureapp/ui/imagedetail/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.droidplusplus.nasapictureapp.ui.imagedetail
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.droidplusplus.nasapictureapp.base.BaseActivity
+import com.droidplusplus.nasapictureapp.databinding.ActivityDetailBinding
+import com.droidplusplus.nasapictureapp.ui.MainViewModel
+import com.droidplusplus.nasapictureapp.ui.MainViewModelFactory
+
+class DetailActivity : BaseActivity<ActivityDetailBinding, MainViewModel>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mActivityViewBinding.root)
+    }
+
+    override fun getViewBing(): ActivityDetailBinding = ActivityDetailBinding.inflate(layoutInflater)
+
+    override fun getViewModel(): MainViewModel =
+        ViewModelProvider(this, MainViewModelFactory()).get(MainViewModel::class.java)
+}
