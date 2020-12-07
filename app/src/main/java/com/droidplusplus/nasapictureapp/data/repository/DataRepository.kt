64865d1ffd87980168055616c1c9c111a3b2520b
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-07 22:53:22 +0530

    DataRepository -> 1. getDataResult() method defined.

--- app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepository.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepository.kt
@@ -1,4 +1,7 @@
 package com.droidplusplus.nasapictureapp.data.repository
 
+import com.droidplusplus.nasapictureapp.data.model.DataItem
+
 interface DataRepository {
+    suspend fun getDataResult(): List<DataItem>?
 }
--- app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepositoryImpl.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepositoryImpl.kt
@@ -1,4 +1,10 @@
 package com.droidplusplus.nasapictureapp.data.repository
 
+import com.droidplusplus.nasapictureapp.data.model.DataItem
+
 class DataRepositoryImpl : DataRepository {
+
+    override suspend fun getDataResult(): List<DataItem>? {
+        TODO("Not yet implemented")
+    }
 }
