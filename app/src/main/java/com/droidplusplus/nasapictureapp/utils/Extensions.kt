f0cb1f0143c89b0049636ae20e494c6af70de7d7
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-07 23:01:38 +0530

    1. utils package created. 2. Extensions file created inside utils package. 3. View extension function defined.

---
+++ app/src/main/java/com/droidplusplus/nasapictureapp/utils/Extensions.kt
@@ -0,0 +1,15 @@
+package com.droidplusplus.nasapictureapp.utils
+
+import android.view.View
+
+/**
+ * View Extensions
+ * handle visibility
+ */
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
