96c14dcd9ebe5577a414d4bc03e429a7ccb8d7f6
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-10 23:23:12 +0530

    1.  initialSetUp and loadData with Complete dataBinding into the recyclerView.

--- app/src/main/java/com/droidplusplus/nasapictureapp/ui/imagelist/MainActivity.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/ui/imagelist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.droidplusplus.nasapictureapp.ui.imagelist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.droidplusplus.nasapictureapp.base.BaseActivity
@@ -7,6 +8,11 @@ import com.droidplusplus.nasapictureapp.data.repository.DataRepositoryImpl
 import com.droidplusplus.nasapictureapp.databinding.ActivityMainBinding
 import com.droidplusplus.nasapictureapp.ui.MainViewModel
 import com.droidplusplus.nasapictureapp.ui.MainViewModelFactory
+import com.droidplusplus.nasapictureapp.ui.imagedetail.DetailActivity
+import com.droidplusplus.nasapictureapp.utils.Constants
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -24,6 +30,36 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mActivityViewBinding.root)
+
+        initialSetUp()
+
+        loadData()
+    }
+
+    private fun loadData() {
+        // Load data from ViewModel gertResult suspend function
+        GlobalScope.launch(Dispatchers.IO) {
+            mViewModel.getDataResult()
+        }
+    }
+
+    private fun initialSetUp() {
+        // RecyclerView adapter setUp
+        mViewBinding.imagesListRV.adapter = mAdapter
+
+        // Adapter listener initialize
+        mAdapter.itemClickListener = { position ->
+            val detailScreenIntent = Intent(this, DetailActivity::class.java)
+            detailScreenIntent.putExtra(Constants.ITEM_POSITION, position)
+            startActivity(detailScreenIntent)
+        }
+
+        // Observe data from ViewModel
+        mViewModel.mDataResultLiveData.observe(this, { result ->
+            result?.takeIf { it.isNotEmpty() }?.let {
+                mAdapter.submitList(result)
+            }
+        })
     }
 
     override fun getViewBing(): ActivityMainBinding = mViewBinding
