a0eb4751f057e0622e3981762e4fd79d1a70eb33
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-10 22:19:45 +0530

    1. stringToList using Gson helper util method created. 2. improvement of readAsString method.

--- app/src/main/java/com/droidplusplus/nasapictureapp/utils/DataUtils.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/utils/DataUtils.kt
@@ -1,5 +1,7 @@
 package com.droidplusplus.nasapictureapp.utils
 
+import com.droidplusplus.nasapictureapp.data.model.DataItem
+import com.google.gson.Gson
 import java.io.IOException
 import java.io.InputStream
 
@@ -8,12 +10,21 @@ import java.io.InputStream
  * @return The text in InputStream as String or null if something went wrong
  */
 suspend fun InputStream.readAsString(): String? {
-    try {
+    return try {
         val availableBytes = ByteArray(available())
         read(availableBytes, 0, availableBytes.size)
-        return String(availableBytes)
+        String(availableBytes)
     } catch (ex: IOException) {
         ex.printStackTrace()
-        return null
+        null
     }
+}
+
+/**
+ * @param str
+ * Converts only string to List of DataItem
+ * @return The List of DataItem
+ */
+suspend fun stringToList(str: String?): List<DataItem> {
+    return Gson().fromJson(str, Array<DataItem>::class.java).toList()
 }
