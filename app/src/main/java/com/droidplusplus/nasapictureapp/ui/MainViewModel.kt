00534c4994bfb0e71320d34acbfe0644e9f26f28
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-10 22:44:41 +0530

    1. getResult suspend method created for getting the data form the repository and postLiveData of result.

--- app/src/main/java/com/droidplusplus/nasapictureapp/ui/MainViewModel.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/ui/MainViewModel.kt
@@ -3,8 +3,10 @@ package com.droidplusplus.nasapictureapp.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.droidplusplus.nasapictureapp.data.model.DataItem
 import com.droidplusplus.nasapictureapp.data.repository.DataRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: DataRepository) : ViewModel() {
 
@@ -13,4 +15,15 @@ class MainViewModel(private val repository: DataRepository) : ViewModel() {
 
     // LiveData for Observe the value
     val mDataResultLiveData: LiveData<List<DataItem>> = _mDataResultLiveData
+
+
+    /**
+     * suspend fun which gets data from the repository and post value of LiveData<List<DataItem>>.
+     */
+    suspend fun getDataResult() {
+        viewModelScope.launch {
+            val result = repository.getDataResult()
+            _mDataResultLiveData.postValue(result)
+        }
+    }
 }
