59c4f78f35f17c2735bb75783c94d4f5ec0551ff
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-10 22:22:07 +0530

    1. getDataResult method implementation (read data from assets and return List<DataItem>).

--- app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepositoryImpl.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/data/repository/DataRepositoryImpl.kt
@@ -1,10 +1,18 @@
 package com.droidplusplus.nasapictureapp.data.repository
 
+import android.content.Context
 import com.droidplusplus.nasapictureapp.data.model.DataItem
+import com.droidplusplus.nasapictureapp.utils.readAsString
+import com.droidplusplus.nasapictureapp.utils.stringToList
+import java.io.InputStream
 
-class DataRepositoryImpl : DataRepository {
+class DataRepositoryImpl(private val context: Context) : DataRepository {
 
     override suspend fun getDataResult(): List<DataItem>? {
-        TODO("Not yet implemented")
+        val inputStream: InputStream = context.assets.open("data.json")
+
+        return inputStream.readAsString()?.let {
+            stringToList(it)
+        } ?: run { null }
     }
 }
