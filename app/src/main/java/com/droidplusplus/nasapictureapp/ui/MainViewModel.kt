df5367ad53f379913f2046a383ca0629c9d0226c
Shailendra <shailendraharinkhede@fitternity.com>
2020-12-10 22:42:00 +0530

    1. Pass DataRepository as parameter into the MainViewModel. 2. LiveData and MutableLiveData crated to observe the value of result.

--- app/src/main/java/com/droidplusplus/nasapictureapp/ui/MainViewModel.kt
+++ app/src/main/java/com/droidplusplus/nasapictureapp/ui/MainViewModel.kt
@@ -1,6 +1,16 @@
 package com.droidplusplus.nasapictureapp.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.droidplusplus.nasapictureapp.data.model.DataItem
+import com.droidplusplus.nasapictureapp.data.repository.DataRepository
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val repository: DataRepository) : ViewModel() {
+
+    // MutableLiveData for post the data result value
+    private val _mDataResultLiveData = MutableLiveData<List<DataItem>>()
+
+    // LiveData for Observe the value
+    val mDataResultLiveData: LiveData<List<DataItem>> = _mDataResultLiveData
 }
